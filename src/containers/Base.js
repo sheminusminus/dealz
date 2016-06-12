4709f33bc6b2782425ddc7468a20931358435a74
sheminusminus <emkolardev@gmail.com>
2016-06-12 15:36:15 -0500

    unit test modules

--- src/containers/Base.js
+++ src/containers/Base.js
@@ -4,7 +4,7 @@ import reactCSS from 'reactcss';
 var Base = React.createClass({
 	render() {
 		return (
-			<h1 className="pageTitle">bargainator.</h1>
+			<h1 className="pageTitle">bargainator</h1>
 		)
 	}
 });
